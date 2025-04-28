59c102dcc2a5d3ab2b6853b4b87c810d38ef6d9d
joseph0x45 <zozozozeph@gmail.com>
2025-04-28 19:30:24 +0000

    add vertical histogram

--- 1-13/main.c
+++ 1-13/main.c
@@ -3,10 +3,36 @@
 #define OUT 0
 #define MAX_LEN 10
 
+void histogram_horizontal(int histogram[MAX_LEN]) {
+  for (int i = 0; i < MAX_LEN; i++) {
+    printf("%d: ", i + 1);
+    for (int j = 0; j < histogram[i]; j++) {
+      printf("*");
+    }
+    printf("\n");
+  }
+}
+void histogram_vertical(int histogram[MAX_LEN], int frequency) {
+  for (int i = frequency; i > 0; i--) {
+    for (int j = 0; j < MAX_LEN; j++) {
+      if (histogram[j] >= i) {
+        printf("* ");
+      } else {
+        printf("  ");
+      }
+    }
+    printf("\n");
+  }
+  for (int i = 0; i < MAX_LEN; i++) {
+    printf("%d ", i + 1);
+  }
+  printf("\n");
+}
+
 int main() {
-  int state, c, current_length;
+  int state, c, current_length, frequency;
   state = OUT;
-  current_length = c = 0;
+  frequency = current_length = c = 0;
   int histogram[MAX_LEN] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
   while ((c = getchar()) != EOF) {
@@ -16,6 +42,9 @@ int main() {
           current_length = MAX_LEN - 1;
         }
         histogram[current_length - 1]++;
+        if (current_length > frequency) {
+          frequency = current_length;
+        }
       }
       state = OUT;
       current_length = 0;
@@ -30,12 +59,7 @@ int main() {
     }
     histogram[current_length - 1]++;
   }
-  for (int i = 0; i < MAX_LEN; i++) {
-    printf("%d: ", i + 1);
-    for (int j = 0; j < histogram[i]; j++) {
-      printf("*");
-    }
-    printf("\n");
-  }
+  /* histogram_horizontal(histogram); */
+  histogram_vertical(histogram, frequency);
   return 0;
 }
